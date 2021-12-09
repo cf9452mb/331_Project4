09c4cf80cce9ab1c0d8e89872341854b8bf11f9b
justinecanlas <75960447+justinecanlas@users.noreply.github.com>
2021-12-08 19:48:23 -0600

    Formatted Comments

--- BPTree.h
+++ BPTree.h
@@ -30,15 +30,15 @@ struct Pair
 //Index B+ Tree node
 class IndexNode {
     private:
-	    ///leaf
+	// Leaf
         bool IS_LEAF;
-		///size of the leaves
+	// Size of the leaves
         int size;
-		///pairings of the leaves
+	// Pairings of the leaves
         Pair *key;
-		/// pointer to the leaves
+	// Pointer to the leaves
         IndexNode **ptr;
-		///BPTree class
+	// BPTree class
         friend class BPTree;
 
    public:
@@ -48,51 +48,61 @@ class IndexNode {
 // B+ Tree Class
 class BPTree {
     private:
-		///root
+	///root
         IndexNode *root;
-		/// @brief Helper function for internal insertion
-		/// @param Pair, IndexNode *, IndexNode *
-		/// @return The specified Pair and nodes are inserted
+	
+	/// @brief Helper function for internal insertion
+	/// @param Pair, IndexNode *, IndexNode *
+	/// @return The specified Pair and nodes are inserted
         void insertInternal(Pair, IndexNode *, IndexNode *);
-		/// @brief The parent of the indexnode is found
-		/// @param IndexNode *, IndexNode *
-		/// @return The parent of the indexnode is returned
+	
+	/// @brief The parent of the indexnode is found
+	/// @param IndexNode *, IndexNode *
+	/// @return The parent of the indexnode is returned
         IndexNode *findParent(IndexNode *, IndexNode *);
+	
         //void insert(Pair);
-		///@brief Helper function to write the IndexNodes to the blocked sequence file
-		///@param IndexNode *, int &, int, int, ofstream &
-		/// @return The indexnodes are written to the blocked sequence file
+	///@brief Helper function to write the IndexNodes to the blocked sequence file
+	///@param IndexNode *, int &, int, int, ofstream &
+	/// @return The indexnodes are written to the blocked sequence file
         void writeNodes(IndexNode *, int &, int, int, ofstream &);
-		/// @brief Find block that the key passed in may be contained in
-		/// @param A int int
-		/// @return The specified block
+	
+	/// @brief Find block that the key passed in may be contained in
+	/// @param A int int
+	/// @return The specified block
         Pair *findBlock(int);
 
    public:
-		/// @brief default constructor for BPTree
+	/// @brief default constructor for BPTree
         BPTree();
-		/// @brief Create a B+ Tree index from the Blocked sequence set file
-		/// @param A string file
-		/// @return The B+Tree index is created
+	
+	/// @brief Create a B+ Tree index from the Blocked sequence set file
+	/// @param A string file
+	/// @return The B+Tree index is created
         void createIndex(string file);
-		/// @brief Writes the indexnodes to the blocked sequence file
-		/// @param A string file
-		/// @return The blocked sequence file with the indexnodes
+	
+	/// @brief Writes the indexnodes to the blocked sequence file
+	/// @param A string file
+	/// @return The blocked sequence file with the indexnodes
         void writeIndex(string file);
-		/// @brief A key is looked up in the blocked/index file
-		/// @param A string string
-		/// @return A key is returned
+	
+	/// @brief A key is looked up in the blocked/index file
+	/// @param A string string
+	/// @return A key is returned
         bool lookUpKey(string);
-		/// @brief The function to print the tree
-		/// @param IndexNode *
-		/// @return The tree is printed
+	
+	/// @brief The function to print the tree
+	/// @param IndexNode *
+	/// @return The tree is printed
         void display(IndexNode *);
-		/// @brief Accessor for the root node
-		/// @return This function returns the root
+	
+	/// @brief Accessor for the root node
+	/// @return This function returns the root
         IndexNode *getRoot();
-		/// @brief Inserting the pair to the B+ Tree
-		/// @param Pair 
-		/// @return The specified pair is inserted to the B+ Tree
+	
+	/// @brief Inserting the pair to the B+ Tree
+	/// @param Pair 
+	/// @return The specified pair is inserted to the B+ Tree
         void insert(Pair);
 };
 
