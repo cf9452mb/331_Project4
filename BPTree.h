116ce0137304d6c189b5b438eeba85cb275afe18
cf9452mb <79483028+cf9452mb@users.noreply.github.com>
2021-12-09 00:11:11 -0600

    Update BPTree.h

--- BPTree.h
+++ BPTree.h
@@ -1,8 +1,21 @@
+//-------------------------------------------------------------
+// BPTree.h
+// Header file for a B+ Tree class
+//
+// Author: Jordan Hebler
+//-------------------------------------------------------------
+// This B+ Tree class can take a blocked sequence set file and
+// create a B+ Tree index. This class can be used for searching
+// and displaying Location objects, sorting Location objects,
+// and adding/deleting records from the blocked/index file.
+//-------------------------------------------------------------
+
 /**
  * @file BPTree.h
  * @brief A B+ Tree class
- * @details This B+ Tree class can take a blocked sequence set file and create a B+ Tree index. This index can be used
- *          for searching and displaying Location objects.
+ * @details This B+ Tree class can take a blocked sequence set file and create a B+ Tree index.
+ *          This class can be used for searching and displaying Location objects, sorting Location
+ *          objects, and adding/deleting records from the blocked/index file.
  *
  * @author Jordan Hebler
  */
@@ -21,90 +34,92 @@
 
 using namespace std;
 
-// Structure used to represent a zip code and RBN pair
+/// @brief Structure used to represent a zip code and RBN pair
 struct Pair
 {
    int zipCode, RBN;
 };
 
-//Index B+ Tree node
+/// @brief Index B+ Tree node
 class IndexNode {
     private:
-	// Leaf
+        /// Determines if this node is a leaf
         bool IS_LEAF;
-	// Size of the leaves
+        /// Size of the Node
         int size;
-	// Pairings of the leaves
+        /// The Node contains Pairs
         Pair *key;
-	// Pointer to the leaves
+        /// Pointers to other Nodes
         IndexNode **ptr;
-	// BPTree class
+        /// friends with the BPTree class
         friend class BPTree;
 
    public:
+        /// @brief constructor
         IndexNode();
 };
 
-// B+ Tree Class
+/// @brief B+ Tree Class
 class BPTree {
     private:
-	///root
+        /// Root Node of the Tree
         IndexNode *root;
-	
-	/// @brief Helper function for internal insertion
-	/// @param Pair, IndexNode *, IndexNode *
-	/// @return The specified Pair and nodes are inserted
+        /// @brief Clear function to reset the Tree
+        void clear();
+        /// @brief Helper function for internal insertion
+        /// @param Pair, IndexNode *, IndexNode *
+        /// @return The specified Pair and nodes are inserted
         void insertInternal(Pair, IndexNode *, IndexNode *);
-	
-	/// @brief The parent of the indexnode is found
-	/// @param IndexNode *, IndexNode *
-	/// @return The parent of the indexnode is returned
+        /// @brief The parent of the indexnode is found
+        /// @param IndexNode *, IndexNode *
+        /// @return The parent of the indexnode is returned
         IndexNode *findParent(IndexNode *, IndexNode *);
-	
-        //void insert(Pair);
-	///@brief Helper function to write the IndexNodes to the blocked sequence file
-	///@param IndexNode *, int &, int, int, ofstream &
-	/// @return The indexnodes are written to the blocked sequence file
-        void writeNodes(IndexNode *, int &, int, int, ofstream &);
-	
-	/// @brief Find block that the key passed in may be contained in
-	/// @param A int int
-	/// @return The specified block
+        /// @brief Writes the indexnodes to the blocked sequence file
+        /// @param A string file
+        /// @return The blocked sequence file with the indexnodes
+        void writeIndex(string file);
+        /// @brief Inserting the pair to the B+ Tree
+        /// @param Pair To be inserted
+        void insert(Pair);
+        /// @brief Helper function to write the IndexNodes to the blocked sequence file
+        /// @param IndexNode *, int &, int, int, ofstream &
+        /// @post The indexnodes are written to the blocked sequence file
+        void writeNodes(IndexNode *, int, int, int, ofstream &);
+        /// @brief Find block that the key passed in may be contained in
+        /// @param A int int
+        /// @return The specified Pair that includes the Block
         Pair *findBlock(int);
 
    public:
-	/// @brief default constructor for BPTree
+        /// @brief constructor
         BPTree();
-	
-	/// @brief Create a B+ Tree index from the Blocked sequence set file
-	/// @param A string file
-	/// @return The B+Tree index is created
-        void createIndex(string file);
-	
-	/// @brief Writes the indexnodes to the blocked sequence file
-	/// @param A string file
-	/// @return The blocked sequence file with the indexnodes
-        void writeIndex(string file);
-	
-	/// @brief A key is looked up in the blocked/index file
-	/// @param A string string
-	/// @return A key is returned
-        bool lookUpKey(string);
-	
-	/// @brief The function to print the tree
-	/// @param IndexNode *
-	/// @return The tree is printed
-        void display(IndexNode *);
-	
-	/// @brief Accessor for the root node
-	/// @return This function returns the root
+        /// @brief Accessor for the root node
+        /// @return This function returns the root Node
         IndexNode *getRoot();
-	
-	/// @brief Inserting the pair to the B+ Tree
-	/// @param Pair 
-	/// @return The specified pair is inserted to the B+ Tree
-        void insert(Pair);
+        /// @brief Create a B+ Tree index from the Blocked sequence set file
+        /// @param A string file
+        /// @post The B+Tree index is created
+        void createIndex(string file);
+        /// @brief A key is looked up in the blocked/index file and outputted if found
+        /// @param string ZipCode to search for
+        /// @post The record corresponding to the key is outputted if found
+        void lookUpKey(string);
+        /// @brief Sort records
+        void sortRecords();
+        /// @brief Outputs the B+ Tree
+        void display(IndexNode *);
+        /// @brief Add new record to the blocked/index file
+        /// @pre Assume the record is compressed to a string with correct format
+        /// @post The new record is added to the blocked/index file
+        /// @param Location A record that is to be added to file
+        void add(Location);
+        /// @brief Delete a record from the blocked/index file
+        /// @post The record is deleted from the file if found
+        /// @param string The key of the record to be deleted
+        void remove(string);
+        
 };
 
 #include "BPTree.cpp"
 #endif
+
